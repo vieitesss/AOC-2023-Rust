8ec3593dfdf1285e38c73d5dc71cd39211058400
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-15 02:30:40 +0100

    add: day 8 ready to go

---
+++ src/day8.rs
@@ -0,0 +1,22 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
+use crate::Solution;
+
+pub struct Day8;
+
+impl Solution for Day8 {
+    type ParsedInput = String;
+
+    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+        todo!()
+    }
+
+    fn part_1(parsed_input: &mut Self::ParsedInput) -> String {
+        todo!()
+    }
+
+    fn part_2(parsed_input: &mut Self::ParsedInput) -> String {
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub mod day4;
 pub mod day5;
 pub mod day6;
 pub mod day7;
+pub mod day8;
 
 
 pub trait Solution {
@@ -67,7 +68,15 @@ pub fn solve_day(day: u8, input: u8) {
             match input {
                 0 => day7::Day7::solve(include_str!("../data/day7/input.txt")),
                 1 => day7::Day7::solve(include_str!("../data/day7/example1.txt")),
-                2 => day7::Day7::solve(include_str!("../data/day7/example2.txt")),
+                2 => day7::Day7::solve(include_str!("../data/day7/example1.txt")),
+                _ => (String::from("Invalid..."), String::from("...input"))
+            };
+        },
+        8 => {
+            match input {
+                0 => day8::Day8::solve(include_str!("../data/day8/input.txt")),
+                1 => day8::Day8::solve(include_str!("../data/day8/example1.txt")),
+                // 2 => day8::Day8::solve(include_str!("../data/day8/example1.txt")),
                 _ => (String::from("Invalid..."), String::from("...input"))
             };
         },
