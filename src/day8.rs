f3d82726ca6c6841672875cdc0c21fc0f24346ca
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-16 11:29:45 +0100

    update: day 7 part 1 rewriten

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,4 @@
-#![allow(dead_code)]
+use std::collections::HashMap;
 
 use regex::Regex;
 
@@ -8,76 +8,41 @@ pub struct Day8;
 
 #[derive(Debug)]
 pub struct Game {
-    instructions: String,
-    nodes: Vec<Node>,
+    instructions: Vec<usize>,
+    nodes: HashMap<String, Vec<String>>,
 }
 
 impl Game {
     // For part 2
-    fn get_start_points(&self) -> Vec<Node> {
-        self.nodes
-            .iter()
-            .filter(|n| n.name.ends_with("A"))
-            .map(|n| n.clone())
-            .collect()
+    fn get_start_points(&self) -> Vec<String> {
+        // self.nodes
+        //     .iter()
+        //     .filter(|n| n.name.ends_with("A"))
+        //     .map(|n| n.clone())
+        //     .collect()
+        vec![]
     }
 
     fn get_steps(&self, node_name: String, ends_z: bool) -> usize {
         let mut steps = 0;
-        let mut index = 0;
-        let mut node = self.find_node(node_name);
-        loop {
-            let next_ins = self.instructions.chars().nth(index).unwrap();
-            let next_node = if next_ins == 'R' {
-                self.find_node(node.right.clone())
-            } else {
-                self.find_node(node.left.clone())
-            };
-            if next_node.is_final_node(next_node.name.clone(), ends_z) {
-                return steps + 1;
-            }
 
+        let mut current_node = node_name.clone();
+        while !self.is_final_node(current_node.clone(), ends_z) {
+            current_node = self.nodes.get(&current_node).unwrap()
+                [self.instructions[steps % self.instructions.len()]]
+            .clone();
             steps += 1;
-            index = (index + 1) % self.instructions.len();
-            node = next_node
         }
+
+        steps
     }
 
-    fn find_node(&self, node_name: String) -> Node {
-        let mut left = 0;
-        let mut right = self.nodes.len() - 1;
-        let mut middle: usize;
-
-        loop {
-            middle = (left + right) / 2;
-
-            let node = self.nodes.iter().nth(middle).unwrap();
-            match node.name.cmp(&node_name) {
-                std::cmp::Ordering::Less => {
-                    left = middle + 1;
-                }
-
-                std::cmp::Ordering::Greater => {
-                    right = middle - 1;
-                }
-                std::cmp::Ordering::Equal => {
-                    return node.clone()
-                }
-            }
+    fn is_final_node(&self, node_name: String, ends_z: bool) -> bool {
+        if ends_z {
+            return node_name.ends_with("Z");
         }
-    }
-}
 
-#[derive(Debug, Clone)]
-struct Node {
-    name: String,
-    left: String,
-    right: String,
-}
-
-impl Node {
-    fn is_final_node(&self, name: String, ends_z: bool) -> bool {
-        return name == "ZZZ" || (ends_z && name.ends_with("Z"));
+        node_name == "ZZZ"
     }
 }
 
@@ -85,19 +50,20 @@ impl Solution for Day8 {
     type ParsedInput = Game;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        let instructions = input_lines.lines().nth(0).unwrap().to_string();
-        let mut nodes: Vec<Node> = vec![];
+        let instructions = input_lines
+            .lines()
+            .nth(0)
+            .unwrap()
+            .chars()
+            .map(|c| if c == 'R' { 1 } else { 0 })
+            .collect::<Vec<usize>>();
+
+        let mut nodes: HashMap<String, Vec<String>> = HashMap::new();
         let re = Regex::new(r"([A-Z]{3})\s=\s\(([A-Z]{3}),\s([A-Z]{3})\)").unwrap();
 
         for (_, [name, left, right]) in re.captures_iter(input_lines).map(|c| c.extract()) {
-            nodes.push(Node {
-                name: name.to_string(),
-                left: left.to_string(),
-                right: right.to_string(),
-            })
+            nodes.insert(name.to_string(), vec![left.to_string(), right.to_string()]);
         }
-
-        nodes.sort_by(|a, b| a.name.cmp(&b.name));
 
         Game {
             instructions,
@@ -106,6 +72,7 @@ impl Solution for Day8 {
     }
 
     fn part_1(parsed_input: &mut Self::ParsedInput) -> String {
+        // println!("{:#?}", parsed_input);
         parsed_input.get_steps("AAA".to_string(), false).to_string()
     }
 
@@ -113,4 +80,3 @@ impl Solution for Day8 {
         "".to_string()
     }
 }
-
