d594cd948bc426b86f31b5068180bf84acc6c589
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-20 13:48:59 +0100

    update: day 10 part 2 done, refactor both parts

--- data/day10/problem.md
+++ data/day10/problem.md
@@ -161,3 +161,136 @@ Find the single giant loop starting at `S`. _How many steps along the loop
 does it take to get from the starting position to the point farthest from the
 starting position?_
 
+## \--- Part Two ---
+
+You quickly reach the farthest point of the loop, but the animal never
+emerges. Maybe its nest is _within the area enclosed by the loop_?
+
+To determine whether it's even worth taking the time to search for such a
+nest, you should calculate how many tiles are contained within the loop. For
+example:
+
+    
+    
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||.....||.
+    .||.....||.
+    .|L-7.F-J|.
+    .|..|.|..|.
+    .L--J.L--J.
+    ...........
+    
+
+The above loop encloses merely _four tiles_ \- the two pairs of `.` in the
+southwest and southeast (marked `I` below). The middle `.` tiles (marked `O`
+below) are _not_ in the loop. Here is the same loop again with those regions
+marked:
+
+    
+    
+    ...........
+    .S-------7.
+    .|F-----7|.
+    .||OOOOO||.
+    .||OOOOO||.
+    .|L-7OF-J|.
+    .|II|O|II|.
+    .L--JOL--J.
+    .....O.....
+    
+
+In fact, there doesn't even need to be a full tile path to the outside for
+tiles to count as outside the loop - squeezing between pipes is also allowed!
+Here, `I` is still within the loop and `O` is still outside the loop:
+
+    
+    
+    ..........
+    .S------7.
+    .|F----7|.
+    .||OOOO||.
+    .||OOOO||.
+    .|L-7F-J|.
+    .|II||II|.
+    .L--JL--J.
+    ..........
+    
+
+In both of the above examples, `_4_` tiles are enclosed by the loop.
+
+Here's a larger example:
+
+    
+    
+    .F----7F7F7F7F-7....
+    .|F--7||||||||FJ....
+    .||.FJ||||||||L7....
+    FJL7L7LJLJ||LJ.L-7..
+    L--J.L7...LJS7F-7L7.
+    ....F-J..F7FJ|L7L7L7
+    ....L7.F7||L7|.L7L7|
+    .....|FJLJ|FJ|F7|.LJ
+    ....FJL-7.||.||||...
+    ....L---J.LJ.LJLJ...
+    
+
+The above sketch has many random bits of ground, some of which are in the loop
+(`I`) and some of which are outside it (`O`):
+
+    
+    
+    _O_ F----7F7F7F7F-7 _OOOO_
+    _O_ |F--7||||||||FJ _OOOO_
+    _O_ || _O_ FJ||||||||L7 _OOOO_
+    FJL7L7LJLJ||LJ _I_ L-7 _OO_
+    L--J _O_ L7 _III_ LJS7F-7L7 _O_
+    _OOOO_ F-J _II_ F7FJ|L7L7L7
+    _OOOO_ L7 _I_ F7||L7| _I_ L7L7|
+    _OOOOO_ |FJLJ|FJ|F7| _O_ LJ
+    _OOOO_ FJL-7 _O_ || _O_ |||| _OOO_
+    _OOOO_ L---J _O_ LJ _O_ LJLJ _OOO_
+    
+
+In this larger example, `_8_` tiles are enclosed by the loop.
+
+Any tile that isn't part of the main loop can count as being enclosed by the
+loop. Here's another example with many bits of junk pipe lying around that
+aren't connected to the main loop at all:
+
+    
+    
+    FF7FSF7F7F7F7F7F---7
+    L|LJ||||||||||||F--J
+    FL-7LJLJ||||||LJL-77
+    F--JF--7||LJLJ7F7FJ-
+    L---JF-JLJ.||-FJLJJ7
+    |F|F-JF---7F7-L7L|7|
+    |FFJF7L7F-JF7|JL---7
+    7-L-JL7||F7|L7F-7F7|
+    L.L7LFJ|||||FJL7||LJ
+    L7JLJL-JLJLJL--JLJ.L
+    
+
+Here are just the tiles that are _enclosed by the loop_ marked with `I`:
+
+    
+    
+    FF7FSF7F7F7F7F7F---7
+    L|LJ||||||||||||F--J
+    FL-7LJLJ||||||LJL-77
+    F--JF--7||LJLJ _I_ F7FJ-
+    L---JF-JLJ _IIII_ FJLJJ7
+    |F|F-JF---7 _III_ L7L|7|
+    |FFJF7L7F-JF7 _II_ L---7
+    7-L-JL7||F7|L7F-7F7|
+    L.L7LFJ|||||FJL7||LJ
+    L7JLJL-JLJLJL--JLJ.L
+    
+
+In this last example, `_10_` tiles are enclosed by the loop.
+
+Figure out whether you have time to search for the nest by calculating the
+area within the loop. _How many tiles are enclosed by the loop?_
+
--- src/day10.rs
+++ src/day10.rs
@@ -1,38 +1,36 @@
-#![allow(unused_variables)]
-#![allow(dead_code)]
-
-use std::fmt::Display;
-
-use phf::{phf_map, Map};
+use std::{cell::LazyCell, cmp::min, collections::HashMap, fmt::Display};
 
 use crate::Solution;
 
 const DIRS: [[i32; 2]; 4] = [[-1, 0], [0, 1], [1, 0], [0, -1]];
 
-static SYMBOLS: Map<char, &'static [u8; 4]> = phf_map! {
-    '|' => &[1, 0, 1, 0],
-    '-' => &[0, 1, 0, 1],
-    'L' => &[1, 1, 0, 0],
-    'J' => &[1, 0, 0, 1],
-    '7' => &[0, 0, 1, 1],
-    'F' => &[0, 1, 1, 0],
-    '.' => &[0, 0, 0, 0],
-    'S' => &[0, 0, 0, 0],
-};
+const SYMBOLS: LazyCell<HashMap<char, &'static [u8; 4]>> = LazyCell::new(|| {
+    let mut h = HashMap::new();
+    h.insert('|', &[1, 0, 1, 0]);
+    h.insert('-', &[0, 1, 0, 1]);
+    h.insert('L', &[1, 1, 0, 0]);
+    h.insert('J', &[1, 0, 0, 1]);
+    h.insert('7', &[0, 0, 1, 1]);
+    h.insert('F', &[0, 1, 1, 0]);
+    h.insert('.', &[0, 0, 0, 0]);
+    h.insert('S', &[0, 0, 0, 0]);
+    h
+});
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct Point(i32, i32);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Board {
     init: Point,
     matrix: Vec<Vec<Vec<u8>>>,
+    road_points: Vec<Point>,
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (i, line) in self.matrix.iter().enumerate() {
-            println!("{:?} ", line);
+        for line in self.matrix.iter() {
+            writeln!(f, "{:?}", line)?;
         }
 
         Ok(())
@@ -40,64 +38,82 @@ impl Display for Board {
 }
 
 impl Board {
-    fn update_point(&mut self, point: Point, dir: usize) {
-        self.matrix
-            .get_mut(point.0 as usize)
-            .unwrap()
-            .get_mut(point.1 as usize)
-            .unwrap()[dir] = 0;
+    fn update_point(&mut self, point: Point, dir: usize, val: u8) {
+        self.matrix[point.0 as usize][point.1 as usize][dir] = val;
     }
 
-    fn get_init_points(&mut self) -> Vec<Point> {
-        DIRS.iter()
-            .enumerate()
-            .map(|(i, [x, y])| {
-                let p = Point(self.init.0.clone() + x, self.init.1 + y);
-                let dir = (i + 2) % 4;
-                if self.is_in_bounds(p.clone()) && self.is_possible_move(p.clone(), dir) {
-                    self.update_point(p.clone(), dir);
-                    return Some(p);
-                }
-                None
-            })
-            .filter(|p| p.is_some())
-            .map(|p| p.unwrap())
-            .collect()
+    fn update_init_dirs(&mut self) {
+        DIRS.iter().enumerate().for_each(|(i, [x, y])| {
+            let p = Point(self.init.0 + x, self.init.1 + y);
+            let dir = (i + 2) % 4;
+            if self.is_in_bounds(&p) && self.is_possible_move(&p, dir) {
+                self.get_point_dirs(&self.init)[i] = 1;
+                self.update_point(self.init, i, 1);
+            }
+        });
     }
 
-    fn is_possible_move(&self, point: Point, dir: usize) -> bool {
-        *self
-            .matrix
-            .get(point.0 as usize)
-            .unwrap()
-            .get(point.1 as usize)
-            .unwrap()
-            .get(dir)
-            .unwrap()
-            == 1
+    fn is_possible_move(&self, point: &Point, dir: usize) -> bool {
+        *self.get_point_dirs(point).get(dir).unwrap() == 1
     }
 
-    fn is_in_bounds(&self, p: Point) -> bool {
+    fn is_in_bounds(&self, p: &Point) -> bool {
         return p.0 >= 0
             && p.0 < self.matrix.len() as i32
             && p.1 >= 0
             && p.1 < self.matrix[0].len() as i32;
     }
 
-    fn get_next_point(&mut self, point: Point) -> Point {
-        let pos = self
-            .matrix
+    fn get_point_dirs(&self, point: &Point) -> Vec<u8> {
+        self.matrix
             .get(point.0 as usize)
             .unwrap()
             .get(point.1 as usize)
             .unwrap()
+            .to_vec()
+    }
+
+    fn is_in_road(&self, point: &Point) -> bool {
+        self.road_points.iter().any(|p| p.eq(point))
+    }
+
+    fn inside_in_line(&self, row: usize, column: usize, up: i32, down: i32, sum: i32) -> i32 {
+        if column == self.matrix[0].len() {
+            return sum;
+        }
+
+        let point = Point(row as i32, column as i32);
+        if self.is_in_road(&point) {
+            let point_dirs = self.get_point_dirs(&point);
+            return self.inside_in_line(
+                row,
+                column + 1,
+                up + point_dirs[0] as i32,
+                down + point_dirs[2] as i32,
+                sum,
+            );
+        } else if min(up, down) % 2 == 1 {
+            return self.inside_in_line(row, column + 1, up, down, sum + 1);
+        }
+
+        self.inside_in_line(row, column + 1, up, down, sum)
+    }
+
+    fn find_road(&mut self, point: &Point, dir: usize, len: usize) -> usize {
+        if point.eq(&self.init) && len > 0 {
+            return len;
+        }
+        self.road_points.push(*point);
+
+        let next_point = Point(point.0 + DIRS[dir][0], point.1 + DIRS[dir][1]);
+        let next_point_dir = self
+            .get_point_dirs(&next_point)
             .iter()
-            .position(|&pos| pos == 1)
+            .enumerate()
+            .position(|(i, d)| i != (dir + 2) % 4 && *d == 1)
             .unwrap();
 
-        let p = Point(point.0 + DIRS[pos][0], point.1 + DIRS[pos][1]);
-        self.update_point(p.clone(), (pos + 2) % 4);
-        p
+        self.find_road(&next_point, next_point_dir, len + 1)
     }
 }
 
@@ -108,6 +124,7 @@ impl Solution for Day10 {
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         let mut init = Point(0, 0);
+        let mut tails = vec![];
         let matrix = input_lines
             .lines()
             .enumerate()
@@ -115,6 +132,9 @@ impl Solution for Day10 {
                 l.chars()
                     .enumerate()
                     .map(|(y, c)| {
+                        if c == '.' {
+                            tails.push(Point(x as i32, y as i32));
+                        }
                         if c == 'S' {
                             init = Point(x as i32, y as i32);
                         }
@@ -124,26 +144,42 @@ impl Solution for Day10 {
             })
             .collect();
 
-        Board { init, matrix }
-    }
-
-    fn part_1(parsed_input: &mut Self::ParsedInput) -> String {
-        let mut points = parsed_input.get_init_points();
-
-        let mut distance = 1;
-        while points[0].0 != points[1].0 || points[0].1 != points[1].1 {
-            points = points
-                .iter()
-                .map(|p| parsed_input.get_next_point(p.clone()))
-                .collect();
-
-            distance += 1;
+        Board {
+            init,
+            matrix,
+            road_points: vec![],
         }
-
-        distance.to_string()
     }
 
-    fn part_2(parsed_input: &mut Self::ParsedInput) -> String {
-        "".to_string()
+    fn part_1(parsed_input: &Self::ParsedInput) -> String {
+        let mut part1 = parsed_input.clone();
+        part1.update_init_dirs();
+        let dir = part1
+            .get_point_dirs(&parsed_input.init)
+            .iter()
+            .position(|d| *d == 1)
+            .unwrap();
+        let road_len = part1.find_road(&parsed_input.init, dir, 0);
+
+        (road_len / 2).to_string()
+    }
+
+    fn part_2(parsed_input: Self::ParsedInput) -> String {
+        let mut part2 = parsed_input.clone();
+        part2.update_init_dirs();
+        let dir = part2
+            .get_point_dirs(&parsed_input.init)
+            .iter()
+            .position(|d| *d == 1)
+            .unwrap();
+        let _ = part2.find_road(&parsed_input.init, dir, 0);
+
+        let total = part2
+            .matrix
+            .iter()
+            .enumerate()
+            .fold(0, |cur, (x, _)| cur + part2.inside_in_line(x, 0, 0, 0, 0));
+
+        total.to_string()
     }
 }
