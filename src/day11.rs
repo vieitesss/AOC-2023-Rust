639996aad8986493363d35fe11e80a4c8f385462
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-21 10:34:14 +0100

    update: minimizing day 11 lines of code

--- src/day11.rs
+++ src/day11.rs
@@ -10,38 +10,35 @@ pub struct Data {
 }
 
 impl Data {
-    fn distance_to_following(
-        &self,
-        galaxy: usize,
-        target: usize,
-        total: i64,
-        increase: i64,
-    ) -> i64 {
+    fn distance_to_following(&self, galaxy: usize, target: usize, total: i64, inc: i64) -> i64 {
         if target == self.galaxies.len() {
             return total;
         }
 
-        let galaxy_point = self.galaxies[galaxy];
-        let target_point = self.galaxies[target];
+        let gp = self.galaxies[galaxy];
+        let tp = self.galaxies[target];
 
-        let mut distance =
-            (target_point.0 - galaxy_point.0) + (target_point.1 - galaxy_point.1).abs();
+        let mut distance = (tp.0 - gp.0) + (tp.1 - gp.1).abs();
 
         for r in self.empty_rows.iter() {
-            if *r > galaxy_point.0 && *r < target_point.0 {
-                distance += increase;
+            if *r > gp.0 && *r < tp.0 {
+                distance += inc;
             }
         }
 
         for c in self.empty_columns.iter() {
-            if *c > galaxy_point.1 && *c < target_point.1
-                || *c < galaxy_point.1 && *c > target_point.1
-            {
-                distance += increase;
+            if *c > gp.1 && *c < tp.1 || *c < gp.1 && *c > tp.1 {
+                distance += inc;
             }
         }
 
-        self.distance_to_following(galaxy, target + 1, total + distance, increase)
+        self.distance_to_following(galaxy, target + 1, total + distance, inc)
+    }
+
+    fn distance_btwn_galaxies(&self, inc: i64) -> i64 {
+        self.galaxies.iter().enumerate().fold(0, |cur, (i, _)| {
+            cur + self.distance_to_following(i, i + 1, 0, inc)
+        })
     }
 }
 
@@ -53,20 +50,18 @@ impl Solution for Day11 {
         let mut empty_rows = vec![];
         let mut empty_columns: Vec<i64> =
             (0..input_lines.lines().count()).map(|i| i as i64).collect();
-        let mut space: i64;
         for (row, line) in input_lines.lines().enumerate() {
-            space = 0;
-            for (col, c) in line.chars().enumerate() {
+            if line.chars().enumerate().fold(0, |cur, (col, c)| {
                 if c == '#' {
                     galaxies.push((row as i64, col as i64));
                     if let Some(index) = empty_columns.iter().position(|c| *c == col as i64) {
                         empty_columns.remove(index);
                     }
-                } else {
-                    space += 1;
+                    return cur;
                 }
-            }
-            if space == line.len() as i64 {
+                cur + 1
+            }) == line.len() as i64
+            {
                 empty_rows.push(row as i64);
             }
         }
@@ -79,24 +74,10 @@ impl Solution for Day11 {
     }
 
     fn part_1(parsed_input: &Self::ParsedInput) -> String {
-        parsed_input
-            .galaxies
-            .iter()
-            .enumerate()
-            .fold(0, |cur, (i, _)| {
-                cur + parsed_input.distance_to_following(i, i + 1, 0, 1)
-            })
-            .to_string()
+        parsed_input.distance_btwn_galaxies(1).to_string()
     }
 
     fn part_2(parsed_input: Self::ParsedInput) -> String {
-        parsed_input
-            .galaxies
-            .iter()
-            .enumerate()
-            .fold(0, |cur, (i, _)| {
-                cur + parsed_input.distance_to_following(i, i + 1, 0, 999_999)
-            })
-            .to_string()
+        parsed_input.distance_btwn_galaxies(999_999).to_string()
     }
 }
