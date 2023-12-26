49dc3570794a8181fb652535f37b87c48547499f
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-26 16:28:28 +0100

    update: day 15 part 1 done

--- src/day15.rs
+++ src/day15.rs
@@ -2,18 +2,48 @@ use crate::Solution;
 
 pub struct Day15;
 
+#[derive(Debug)]
+struct Step(String);
+
+impl Step {
+    fn get_value(&self) -> usize {
+        let mut total = 0;
+        for ch in self.0.chars() {
+            total += ch as usize;
+            total *= 17;
+            total %= 256;
+        }
+
+        total
+    }
+}
+
+#[derive(Debug)]
+pub struct Steps(Vec<Step>);
+
+impl Steps {
+    fn get_total_sum(&self) -> usize {
+        self.0.iter().map(|step| step.get_value()).sum()
+    }
+}
+
 impl Solution for Day15 {
-    type ParsedInput = String;
+    type ParsedInput = Steps;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        todo!()
+        Steps(
+            input_lines
+                .split(",")
+                .map(|step| Step(step.trim().to_string()))
+                .collect(),
+        )
     }
 
     fn part_1(parsed_input: &Self::ParsedInput) -> String {
-        todo!()
+        parsed_input.get_total_sum().to_string()
     }
 
-    fn part_2(parsed_input: Self::ParsedInput) -> String {
-        todo!()
+    fn part_2(_parsed_input: Self::ParsedInput) -> String {
+        "".to_string()
     }
 }
