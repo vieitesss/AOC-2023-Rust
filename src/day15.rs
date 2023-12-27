67b91e83ada4e15a187b3ade338e4ab89401ba03
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-27 11:55:52 +0100

    update: day 15 part 2 done

--- src/day15.rs
+++ src/day15.rs
@@ -1,6 +1,4 @@
-#![allow(dead_code)]
-
-use std::{collections::HashMap, fmt::Display};
+use std::collections::HashMap;
 
 use crate::Solution;
 
@@ -18,17 +16,6 @@ struct Step {
     label: String,
     operation: OP,
     value: Option<usize>,
-}
-
-impl Display for Step {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "{} {} {:?} {:?}",
-            self.def, self.label, self.operation, self.value
-        )?;
-        Ok(())
-    }
 }
 
 impl Step {
@@ -57,20 +44,6 @@ impl Step {
 #[derive(Debug)]
 pub struct Steps(Vec<Step>);
 
-impl Display for Steps {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for (i, step) in self.0.iter().enumerate() {
-            if i == self.0.len() - 1 {
-                write!(f, "{}", step)?;
-                break;
-            }
-            writeln!(f, "{}", step)?;
-        }
-
-        Ok(())
-    }
-}
-
 impl Steps {
     fn get_total_sum(&self) -> usize {
         self.0.iter().map(|step| step.get_value()).sum()
@@ -82,7 +55,8 @@ type Box = Vec<(String, Option<usize>)>;
 impl Solution for Day15 {
     type ParsedInput = Steps;
 
-    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+    fn parse_input(mut input_lines: &str) -> Self::ParsedInput {
+        input_lines = input_lines.trim();
         Steps(
             input_lines
                 .split(",")
@@ -113,10 +87,8 @@ impl Solution for Day15 {
     }
 
     fn part_2(parsed_input: Self::ParsedInput) -> String {
-        // <Box_number, Box>
         let mut boxes: HashMap<usize, Box> = HashMap::new();
         parsed_input.0.iter().for_each(|step| {
-            // println!("    {step}");
             let op = &step.operation;
             let b = step.get_box();
 
@@ -143,14 +115,11 @@ impl Solution for Day15 {
             } else if *op == OP::ADD {
                 boxes.insert(b, vec![step.get_id()]);
             }
-
-            // println!("{:?}", boxes);
         });
 
         let mut total = 0;
         for (n, boxx) in boxes.iter() {
             for (slot, lens) in boxx.iter().enumerate() {
-                // * `rn`: `1` (box 0) * `1` (first slot) * `1` (focal length) = `_1_`
                 total += (n + 1) * (slot + 1) * lens.1.unwrap();
             }
         }
