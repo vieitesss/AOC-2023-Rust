c0ba1263ba36b9de07fad13cc3d5569556eb595c
Daniel Vieites Torres <dvieitest@gmail.com>
2023-12-27 19:30:56 +0100

    add: day 17 ready to go

---
+++ src/day17.rs
@@ -0,0 +1,19 @@
+use crate::Solution;
+
+pub struct Day17;
+
+impl Solution for Day17 {
+    type ParsedInput = String;
+
+    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+        todo!()
+    }
+
+    fn part_1(parsed_input: &Self::ParsedInput) -> String {
+        todo!()
+    }
+
+    fn part_2(parsed_input: Self::ParsedInput) -> String {
+        todo!()
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ pub mod day13;
 pub mod day14;
 pub mod day15;
 pub mod day16;
+pub mod day17;
 pub mod day2;
 pub mod day3;
 pub mod day4;
@@ -153,7 +154,6 @@ pub fn solve_day(day: u8, input: u8) {
             match input {
                 0 => day15::Day15::solve(include_str!("../data/day15/input.txt")),
                 1 => day15::Day15::solve(include_str!("../data/day15/example1.txt")),
-                // 2 => day15::Day15::solve(include_str!("../data/day15/example2.txt")),
                 _ => (String::from("Invalid..."), String::from("...input")),
             };
         }
@@ -161,7 +161,14 @@ pub fn solve_day(day: u8, input: u8) {
             match input {
                 0 => day16::Day16::solve(include_str!("../data/day16/input.txt")),
                 1 => day16::Day16::solve(include_str!("../data/day16/example1.txt")),
-                // 2 => day16::Day16::solve(include_str!("../data/day16/example2.txt")),
+                _ => (String::from("Invalid..."), String::from("...input")),
+            };
+        }
+        17 => {
+            match input {
+                0 => day17::Day17::solve(include_str!("../data/day17/input.txt")),
+                1 => day17::Day17::solve(include_str!("../data/day17/example1.txt")),
+                // 2 => day17::Day17::solve(include_str!("../data/day17/example2.txt")),
                 _ => (String::from("Invalid..."), String::from("...input")),
             };
         }
